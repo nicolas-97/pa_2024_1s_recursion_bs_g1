e857a6a4a27effa8db711e51660e8f0d661e565e
Nicolas Leiton <nicolas.leitonp@utadeo.edu.co>
2024-02-14 14:36:29 -0500

    Prueba 1 binary search

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -3,8 +3,25 @@ from models.tree_node import TreeNode
 '''
     Implementa una función de búsqueda binaria que encuentre un elemento en una lista ordenada de enteros y retorne su posicion si no lo encuntra que retorne -1
 '''
-def binary_search(arr: list, target: int):
-    return 1
+def binary_search(arr: list, target: int, ini=0, fin=0):
+    if fin==0:
+        fin=len(arr)
+
+    size = fin-ini
+    if size <=1:
+        if arr[0]==target:
+            return 0
+        else:
+            return -1
+    else:
+        mitad = size//2 + ini
+        if arr[mitad]==target:
+            return mitad
+        elif arr[mitad]>target:
+            return binary_search(arr, target, ini, mitad)
+        else:
+            return binary_search(arr, target, mitad, fin)
+
 
 '''
     Implementa una función de búsqueda binaria para encontrar un elemento en una matriz ordenada (fila y columna) de enteros y retorne verdadero si la encuntra el elmento o falso si no lo encuentra
