b16f0bcf3e542c7e5896ce06845a4eeed58a9da9
Nicolas Leiton <nicolas.leitonp@utadeo.edu.co>
2024-02-14 14:10:22 -0500

    Pruebas de recursion

--- algorithms/recursion.py
+++ algorithms/recursion.py
@@ -2,28 +2,57 @@
     Implementa una función recursiva para calcular el factorial de un número entero positivo
 '''
 def factorial(n):
-    return n
+    if n<=1:
+        return 1
+    else:
+        return n * factorial(n-1)
 
 '''
     Implementa una función recursiva para calcular el término n-ésimo de la secuencia de Fibonacci.
 '''
 def fibonacci(n):
-    return n
+    if n==0:
+        return 0
+    elif n==1 or n==2:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+    
 
 '''
     Implementa una función recursiva para calcular la suma de los primeros n números enteros.
 '''
 def sum_of_numbers(n):
-    return n
+    if n<=0:
+        return 0
+    else:
+        return n+sum_of_numbers(n-1)
+
+
 
 '''
     Implementa una función recursiva para calcular a^n (a elevado a la potencia n).
 '''
 def power(a, n):
-    return a + n
+    if n==0:
+        return 0
+    elif n==1:
+        return a
+    else:
+        return a* power(a, n-1)
 
 '''
     Implementa una función recursiva para encontrar el máximo elemento en una lista de enteros.
 '''
 def max_in_list(lst):
-    return lst
+    size= len(lst)
+    if size==0:
+        return lst
+    elif size==1:
+        return lst[0]
+    else:
+        if lst[0]<lst[1]:
+            lst.pop(1)
+            return max_in_list(lst)
+        else:
+            return max_in_list(lst[1:])
