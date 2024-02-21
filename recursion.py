2816ba9d3cff917713f466508e505f0373521fcb
mafemoreno25 <159285330+mafemoreno25@users.noreply.github.com>
2024-02-20 23:32:59 -0500

    Add files via upload

--- recursion.py
+++ recursion.py
@@ -14,12 +14,10 @@ def factorial(n):
 '''
 def fibonacci(n):
     return n
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
+    if n <= 1:
+        return n
     else:
-        return fibonacci(n-1)+fibonacci(n-2)
+        return fibonacci(n-1) + fibonacci(n-2)
 
 '''
     Implementa una función recursiva para calcular la suma de los primeros n números enteros.
