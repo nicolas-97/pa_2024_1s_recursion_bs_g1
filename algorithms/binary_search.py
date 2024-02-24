6a90d2fa8416eebe9dfa25928853b2c9d5b82f26
Andres Alaix <80136164+alaixgg@users.noreply.github.com>
2024-02-24 11:36:47 -0500

    Update binary_search.py
    "error definicion treenode"

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -1,4 +1,5 @@
 from models.tree_node import TreeNode
+import numpy as np
 
 '''
     Implementa una función de búsqueda binaria que encuentre un elemento en una lista ordenada de enteros y retorne su posicion si no lo encuntra que retorne -1
@@ -21,12 +22,27 @@ def binary_search(arr: list, target: int, start=0, end=0):
 '''
     Implementa una función de búsqueda binaria para encontrar un elemento en una matriz ordenada (fila y columna) de enteros y retorne verdadero si la encuntra el elmento o falso si no lo encuentra
 '''
-def binary_search_matrix(matrix: list[list[int]], target: int, start=0, end=0):
-   
-
+def binary_search_matrix(matrix: list[list[int]], target: int):
+     vector_uni= np.concatenate(matrix)
+     binary_search(vector_uni)
 
 '''
     Implementa una función de búsqueda binaria en un árbol binario de búsqueda  y retorne verdadero si la encuntra el elmento o falso si no lo encuentra
 '''
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        
 def binary_search_tree(root: TreeNode, target: int):
-    return False
+    if root is None:
+        return False
+    elif root.val ==target :
+        return True
+    elif root.val < target :
+        return binary_search_tree(root.right,target)
+    elif root.val >target :
+        return binary_search_tree(root.left,target)
+
+
